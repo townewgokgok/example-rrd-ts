14bc9a5fdd634ce3991d8c76764eebf9052ed730
townewgokgok <townewgokgok@gmail.com>
2017-03-11 22:53:25 +0900

    rrd create

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs-promise';
 import * as yaml from 'js-yaml';
 import * as amqp from 'amqplib';
+const RRD: any = require('rrd').RRD;
 
 interface Settings {
 	amqp: {
@@ -22,17 +23,43 @@ interface RrdRequest {
 	values: number[];
 }
 
+function rrdCreate(rrd:any, params:string[]): Promise<any> {
+	return new Promise((resolve, reject) => {
+		rrd.create(params, {}, (err:string) => {
+			if (err) reject(err); else resolve();
+		});
+	});
+}
+
+let s: Settings;
+
+async function processRequest(req: RrdRequest) {
+	let rrd = new RRD(s.rrd.file_path);
+	await rrdCreate(rrd, [
+		`DS:value1:GAUGE:${s.rrd.heartbeat}:U:U`,
+		`DS:value2:GAUGE:${s.rrd.heartbeat}:U:U`,
+		`DS:value3:GAUGE:${s.rrd.heartbeat}:U:U`,
+		`RRA:AVERAGE:0.5:1:60`
+	]);
+}
+
+function onAmqpMessage(msg: amqp.Message) {
+	let req: RrdRequest = JSON.parse(msg.content.toString());
+	console.log(" [x] Received", req);
+	processRequest(req).catch(err => {
+		console.log(err);
+	});
+}
+
 async function main() {
 	const yamlSrc = await fs.readFile('settings.yml');
-	const s: Settings = yaml.safeLoad(yamlSrc.toString());
+	s = yaml.safeLoad(yamlSrc.toString());
 	const url = `amqp://${s.amqp.user}:${s.amqp.pass}@${s.amqp.host}:${s.amqp.port}`;
 	console.log(`Connecting to ${url}`);
 	let conn = await amqp.connect(url);
 	let ch = await conn.createChannel();
 	ch.assertQueue(s.amqp.queue, {durable: true});
-	await ch.consume(s.amqp.queue, msg => {
-		console.log(" [x] Received %s", msg.content.toString());
-	}, {noAck: false});
+	await ch.consume(s.amqp.queue, onAmqpMessage, {noAck: true});
 }
 
 main().catch(err => {
